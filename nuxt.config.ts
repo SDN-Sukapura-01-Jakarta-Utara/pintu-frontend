ac68f0d5ca3baa3583ba6d6ff9098c8b3b482861
SyahiraIsnaeni <syahiraisnaeni15@gmail.com>
2026-02-12 10:44:57 +0700

    add fonts poppins

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,6 +4,8 @@ export default defineNuxtConfig({
   compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
   css: ['./app/assets/css/main.css'],
+  pages: true,
+  layouts: true,
   vite: {
     plugins: [
       tailwindcss(),
