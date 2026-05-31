543c414b94198e404f9a77459782f493f8d33037
SyahiraIsnaeni <syahiraisnaeni15@gmail.com>
2026-05-31 14:18:23 +0700

    add title and icon on login page

--- app/pages/backoffice/login.vue
+++ app/pages/backoffice/login.vue
@@ -7,4 +7,15 @@ definePageMeta({
   layout: 'backoffice-layout',
   middleware: 'auth'
 })
+
+useHead({
+  title: 'Login | PINTU SDN Sukapura 01',
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/jpeg',
+      href: '/logo-sekolah.jpg'
+    }
+  ]
+})
 </script>
